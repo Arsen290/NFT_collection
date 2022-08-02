75c2d9a29cdeebb883ec6bbc3db0e13e3f808279
Arsen290 <arsen290@ukr.net>
2022-08-02 23:00:54 +0200

    Make Slider + button Add

--- collection/urls.py
+++ collection/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as authViews
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('profile', views.profile, name="profile"),
+    path('profile/', views.profile, name="profile"),
 
 ]
